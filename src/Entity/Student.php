eb281e94a100fc02a7ecae5637589be9029b6de8
Julien Fuchez <julien.fuchez@gmail.com>
2020-01-30 12:09:14 +0100

    Create entity and repo Student

---
+++ src/Entity/Student.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\StudentRepository")
+ */
+class Student
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $uuid;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $firstname;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $lastname;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $birthDate;
+
+    /**
+     * @ORM\Column(type="smallint")
+     */
+    private $gender;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=2, nullable=true)
+     */
+    private $firstMark;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=2, nullable=true)
+     */
+    private $seondMark;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $createdDate;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $updatedDate;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getBirthDate(): ?\DateTimeImmutable
+    {
+        return $this->birthDate;
+    }
+
+    public function setBirthDate(\DateTimeImmutable $birthDate): self
+    {
+        $this->birthDate = $birthDate;
+
+        return $this;
+    }
+
+    public function getGender(): ?int
+    {
+        return $this->gender;
+    }
+
+    public function setGender(int $gender): self
+    {
+        $this->gender = $gender;
+
+        return $this;
+    }
+
+    public function getFirstMark(): ?float
+    {
+        return $this->firstMark;
+    }
+
+    public function setFirstMark(?float $firstMark): self
+    {
+        $this->firstMark = $firstMark;
+
+        return $this;
+    }
+
+    public function getSeondMark(): ?float
+    {
+        return $this->seondMark;
+    }
+
+    public function setSeondMark(?float $seondMark): self
+    {
+        $this->seondMark = $seondMark;
+
+        return $this;
+    }
+
+    public function getCreatedDate(): ?\DateTimeImmutable
+    {
+        return $this->createdDate;
+    }
+
+    public function setCreatedDate(\DateTimeImmutable $createdDate): self
+    {
+        $this->createdDate = $createdDate;
+
+        return $this;
+    }
+
+    public function getUpdatedDate(): ?\DateTimeInterface
+    {
+        return $this->updatedDate;
+    }
+
+    public function setUpdatedDate(?\DateTimeInterface $updatedDate): self
+    {
+        $this->updatedDate = $updatedDate;
+
+        return $this;
+    }
+}

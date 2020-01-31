f8055f636bdb785e7d37b53e338290d67aad5089
Gaëtan Rolé-Dubruille <gaetan.role-dubruille@epitech.eu>
2020-01-31 14:40:27 +0100

    Improving CRUD views and controller/form.

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -5,28 +5,36 @@ namespace App\Controller;
 use App\Entity\Student;
 use App\Form\StudentType;
 use App\Repository\StudentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/student")
+ * @author EducManagement <educ.management@domain.fr>
+ * @Route("/student", name="app_student_")
  */
-class StudentController extends AbstractController
+final class StudentController extends AbstractController
 {
-    /**
-     * @Route("/", name="student_index", methods={"GET"})
-     */
-    public function index(StudentRepository $studentRepository): Response
+    /** @var EntityManagerInterface */
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('student/index.html.twig', [
-            'students' => $studentRepository->findAll(),
-        ]);
+        $this->entityManager = $entityManager;
     }
 
     /**
-     * @Route("/new", name="student_new", methods={"GET","POST"})
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(StudentRepository $studentRepository): Response
+    {
+        return $this->render('student/index.html.twig', ['students' => $studentRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
     {
@@ -35,31 +43,25 @@ class StudentController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($student);
-            $entityManager->flush();
+            $this->entityManager->persist($student);
+            $this->entityManager->flush();
 
-            return $this->redirectToRoute('student_index');
+            return $this->redirectToRoute('app_student_index');
         }
 
-        return $this->render('student/new.html.twig', [
-            'student' => $student,
-            'form' => $form->createView(),
-        ]);
+        return $this->render('student/new.html.twig', ['student' => $student, 'form' => $form->createView()]);
     }
 
     /**
-     * @Route("/{id}", name="student_show", methods={"GET"})
+     * @Route("/{uuid}", name="show", methods={"GET"})
      */
     public function show(Student $student): Response
     {
-        return $this->render('student/show.html.twig', [
-            'student' => $student,
-        ]);
+        return $this->render('student/show.html.twig', ['student' => $student]);
     }
 
     /**
-     * @Route("/{id}/edit", name="student_edit", methods={"GET","POST"})
+     * @Route("/{uuid}/edit", name="edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Student $student): Response
     {
@@ -67,28 +69,24 @@ class StudentController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $this->entityManager->flush();
 
-            return $this->redirectToRoute('student_index');
+            return $this->redirectToRoute('app_student_index');
         }
 
-        return $this->render('student/edit.html.twig', [
-            'student' => $student,
-            'form' => $form->createView(),
-        ]);
+        return $this->render('student/edit.html.twig', ['student' => $student, 'form' => $form->createView()]);
     }
 
     /**
-     * @Route("/{id}", name="student_delete", methods={"DELETE"})
+     * @Route("/{uuid}", name="delete", methods={"DELETE"})
      */
     public function delete(Request $request, Student $student): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$student->getId(), $request->request->get('_token'))) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($student);
-            $entityManager->flush();
+        if ($this->isCsrfTokenValid('delete'.$student->getUuid()->toString(), $request->request->get('_token'))) {
+            $this->entityManager->remove($student);
+            $this->entityManager->flush();
         }
 
-        return $this->redirectToRoute('student_index');
+        return $this->redirectToRoute('app_student_index');
     }
 }
--- src/Form/StudentType.php
+++ src/Form/StudentType.php
@@ -2,33 +2,49 @@
 
 namespace App\Form;
 
+use App\Entity\Classroom;
 use App\Entity\Student;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class StudentType extends AbstractType
+/**
+ * @author EducManagement <educ.management@domain.fr>
+ */
+final class StudentType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstName')
-            ->add('lastName')
-            ->add('birthDate')
-            ->add('gender')
-            ->add('firstMark')
-            ->add('secondMark')
-            ->add('uuid')
-            ->add('createdAt')
-            ->add('updatedAt')
-            ->add('classroom')
+            ->add('firstName', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Write your firstname here.'
+                ]
+            ])
+            ->add('lastName', TextType::class)
+            ->add('birthDate', BirthdayType::class, [
+                'input' => 'datetime_immutable',
+            ])
+            ->add('gender', ChoiceType::class, [
+                'choices' => ['Boy' => false, 'Girl' => true]
+            ])
+            ->add('firstMark', IntegerType::class)
+            ->add('secondMark', IntegerType::class)
+            ->add('classroom', EntityType::class, [
+                'class' => Classroom::class,
+                'choice_label' => 'name'
+            ])
         ;
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
-        $resolver->setDefaults([
-            'data_class' => Student::class,
-        ]);
+        $resolver->setDefaults(['data_class' => Student::class]);
     }
 }
